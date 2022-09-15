2066821349b9fd1420947b9056f5915953f3a765
Kristi Rajandu <kristi.kutt@khk.ee>
2022-09-15 09:51:04 +0300

    Merge remote-tracking branch 'Java-vs21/master'

--- week6/Library.java
+++ week6/Library.java
@@ -3,17 +3,18 @@ package week6;
 import java.util.ArrayList;
 
 public class Library {
-
     private ArrayList<Book> books;
 
-    public Library() { this.books = new ArrayList<Book>();
+    public Library() {
+        this.books = new ArrayList<Book>();
     }
 
     public void addBook(Book newBook){
-        this.books.add(newBook);}
+        this.books.add(newBook);
+    }
 
     public void printBooks(){
-        for(Book book:this.books){
+        for (Book book: this.books) {
             System.out.println(book);
         }
     }
