d4dbe959624d46e41635157f385c89dcfffca7be
Kristi Rajandu <kristi.kutt@khk.ee>
2022-09-15 11:43:14 +0300

    Merge remote-tracking branch 'Java-vs21/master'

---
+++ week6/Ex3.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class Ex3 {
+    public static void main(String[] args) {
+
+        char operator;
+        Double number1, number2, result;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Mida soovid teha: '+',  '-',  '*', või / '/'");
+        operator = input.next().charAt(0);
+
+        System.out.println("Sisesta esimene number");
+        number1 = input.nextDouble();
+
+        System.out.println("Sisesta teine number");
+        number2 = input.nextDouble();
+
+        switch (operator) {
+
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+
+
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+
+
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+
+
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+
+            default:
+                System.out.println("Midagi läks valesti. :(");
+                break;
+        }
+
+        input.close();
+    }
+}
