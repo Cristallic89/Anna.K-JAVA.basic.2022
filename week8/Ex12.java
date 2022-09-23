fbc341bfe549a8053c88272c0bcf73424328b647
Kristi Rajandu <kristi.kutt@khk.ee>
2022-09-23 08:50:56 +0300

    Tööd

--- week8/Ex12.java
+++ week8/Ex12.java
@@ -1,13 +1,51 @@
 package week8;
 
-public class Ex12 {
-    public static void main(String[] args) {
-        ShoppingBasket basket = new ShoppingBasket();
-        basket.add("milk", 3);
-        basket.add("buttermilk", 2);
-        basket.add("cheese", 5);
-        System.out.println("basket price: " + basket.price());
-        basket.add("computer", 899);
-        System.out.println("basket price: " + basket.price());
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Storehouse {
+    private Map<String, Integer> products;
+    private Map<String, Integer> stocks;
+
+    public Storehouse() {
+        products = new HashMap<String, Integer>();
+        stocks = new HashMap<String, Integer>();
+    }
+
+    public void addProduct(String product, int price, int stock){
+        this.products.put(product, price);
+        this.stocks.put(product, stock);
+    }
+
+    public int price(String product){
+        if(this.products.containsKey(product)){
+            return this.products.get(product);
+        } else {
+            return -99;
+        }
+    }
+
+    public int stock(String product){
+        if(this.stocks.containsKey(product)){
+            return this.stocks.get(product);
+        } else {
+            return 0;
+        }
+    }
+
+    public boolean take(String product){
+        if(this.stocks.containsKey(product)){
+            if(this.stocks.get(product) > 0){
+                this.stocks.put(product, this.stocks.get(product) - 1);
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public Set<String> products(){
+        return this.products.keySet();
     }
 }
--- week8/Purchase.java
+++ week8/Purchase.java
@@ -1,25 +1,52 @@
 package week8;
 
 public class Purchase {
-
     private String product;
-    public int amount;
-    public int unitPrice;
+    private int amount;
+    private int unitPrice;
 
-
-    public Purchase(String product, int amount, int price) {
-        this.product = this.product;
+    public Purchase(String product, int amount, int unitPrice) {
+        this.product = product;
         this.amount = amount;
         this.unitPrice = unitPrice;
     }
+
+    public String getProduct() {
+        return product;
+    }
+
     public int price(){
-        return this.unitPrice * this.amount();
+        return this.unitPrice * this.amount;
     }
 
-    private int amount() {
-        amount = 1;
-        return 0;
+    public void increaseAmount(){
+        this.amount++;
     }
 
+    @Override
+    public String toString() {
+        return this.product + ": " +this.amount;
+    }
 
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        if (getClass() != object.getClass()) {
+            return false;
+        }
+
+        Purchase compared = (Purchase) object;
+
+        if (!this.product.equals(compared.product)) {
+            return false;
+        }
+
+        if (this.price() != compared.price()) {
+            return false;
+        }
+
+        return true;
+    }
 }
