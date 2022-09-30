4339be9bea1c639afd02cacc7baa8d9ce0ab3a3a
Kristi Rajandu <kristi.kutt@khk.ee>
2022-09-30 11:39:17 +0300

    Kohvimasin

--- week10/CoffeeMachine.java
+++ week10/CoffeeMachine.java
@@ -4,99 +4,121 @@ package week10;
 import java.util.Scanner;
 class CoffeeMachine{
     static Scanner scan = new Scanner(System.in);
-    private double coffee_powder, milk, water;
+    private double kohvi_puru, piim, vesi;
     private int Coffee_Count = 0;
-    public CoffeeMachine(){ //Default Constructor Initialization...
-        this.coffee_powder=0;
-        this.milk=0;
-        this.water=0;
+    public CoffeeMachine(){ //loome komponentide valiku...
+        this.kohvi_puru=0;
+        this.piim=0;
+        this.vesi=0;
     }
-    private void SetIngredient(){   //Calling for Filling Ingredient...
-        System.out.println("\nFilling...");
-        this.coffee_powder = 500.0;
-        this.milk = 1;
-        this.water= 2;
-        System.out.println("Filling Completed.");
+    private void SetIngredient(){   //kutsume esile täitmise ja palju 1 korra sees täidetakse...
+        System.out.println("\nTäidan...");
+        this.kohvi_puru = 700.0;
+        this.piim = 1;
+        this.vesi= 2;
+        System.out.println("Täitmine valmis.");
     }
-    private void GetIngredient(){   //To Get Status
-        System.out.println("Available Coffee Power(Gram) "+String.format("%.1f",this.coffee_powder));
-        System.out.println("Available Milk(Liter) "+String.format("%.1f", this.milk));
-        System.out.println("Available Water(Liter) "+String.format("%.1f", this.water));
+    private void GetIngredient(){   //Et saada teada staatust...
+        System.out.println("Kohvipuru on veel(Grammi) "+String.format("%.1f",this.kohvi_puru));
+        System.out.println("Piima on veel(Liitrit) "+String.format("%.1f", this.piim));
+        System.out.println("Vett on veel(Liitrit) "+String.format("%.1f", this.vesi));
     }
-    private void CleanMachine(){    //Initialization with Null In order to Clean Machine
-        System.out.println("\nCleaning Machine...");
-        this.coffee_powder = 0;
-        this.milk = 0;
-        this.water= 0;
-        System.out.println("Cleaning Completed.");
+    private void CleanMachine(){    //Nullime kõik ära(masin on pärast seda tühi)
+        System.out.println("\nPuhastan masinat...");
+        this.kohvi_puru = 0;
+        this.piim = 0;
+        this.vesi= 0;
+        System.out.println("Puhastus valmis.");
     }
     private void makecoffee(){  //Coffee Selection Menu
         System.out.println("\n ------------------ ");
-        System.out.println("|   Select Type:   |\n ------------------ \n| 1:  Black Coffee |\n| 2:  Milk Coffee  |\n| 0   to Discard   |");
+        System.out.println("|   Vali millist kohvi soovid:   |\n ------------------ \n| 1:  Musta kohvi |\n| 2:  Piimaga kohvi  |\n| 3: Espressot |\n|  0   Ei tahagi   |");
         System.out.println(" ------------------ \n");
         char t = scan.next().charAt(0);
         switch(t){
             case '1':
-                this.BlackCoffee();     //Call to BlackCoffee Method
+                this.BlackCoffee();     //Kutsub musta kohvi meetodi
                 break;
             case '2':
-                this.MilkCoffee();      //Call to MilkCoffee Method
+                this.MilkCoffee();      //Kutsub piimakohvi meetodi
+                break;
+            case '3':
+                this.Espresso();      //Kutsub espresso meetodi
                 break;
             case '0':
                 break;
         }
     }
     private void BlackCoffee(){
-        if(this.coffee_powder >= 10 && this.water >= 0.2){
-            System.out.println("\nMaking Black Coffee...");
-            System.out.println("\nTaking 10gm of Coffee Powder.");
-            this.coffee_powder = this.coffee_powder - 10;
-            System.out.println("Taking 0.2 liter of Water.");
-            this.water = this.water - 0.2;
-            System.out.println("\nYour Black Coffee is Ready.");
+        if(this.kohvi_puru >= 10 && this.vesi >= 0.2){
+            System.out.println("\nTeen musta kohvi...");
+            System.out.println("\nVõtan 10gm kohvipuru.");
+            this.kohvi_puru = this.kohvi_puru - 10;
+            System.out.println("Võtan 0.2 liitrit vett.");
+            this.vesi = this.vesi - 0.2;
+            System.out.println("\nSinu must kohv on valmis.");
             this.Coffee_Count++;
         }
         else{
-            System.out.println("\nAvailable Coffee Power(Gram) "+String.format("%.1f",this.coffee_powder));
-            System.out.println("Available Water(Liter) "+String.format("%.1f", this.water));
-            System.out.println("\nSome Iteams Are Not Available, Please Fill before Making Coffee.");
+            System.out.println("\nKohvipuru on veel(grammi) "+String.format("%.1f",this.kohvi_puru));
+            System.out.println("Vett on veel(Liitrid) "+String.format("%.1f", this.vesi));
+            System.out.println("\nMõned komponendid on otsas, Palun täida enne kui kohvi teed.");
         }
     }
-    private void MilkCoffee(){
-        if(this.coffee_powder >= 10 && this.milk >= 0.4 && this.water >= 0.2){
-            System.out.println("\nMaking Milk Coffee...");
-            System.out.println("\nTaking 10gm of Coffee Powder.");
-            this.coffee_powder = this.coffee_powder - 10;
-            System.out.println("Taking 0.4 Liter of Milk.");
-            this.milk = this.milk - 0.4;
-            System.out.println("Taking 0.2 liter of Water.");
-            this.water = this.water - 0.2;
-            System.out.println("\nYour Milk Coffee is Ready.");
+
+
+    private void Espresso(){
+        if(this.kohvi_puru >= 10 && this.vesi >= 0.2){
+            System.out.println("\nTeen espressot...");
+            System.out.println("\nVõtan 40gm kohvipuru.");
+            this.kohvi_puru = this.kohvi_puru - 10;
+            System.out.println("Võtan 0.2 liitrit vett.");
+            this.vesi = this.vesi - 0.2;
+            System.out.println("\nSinu espresso.");
             this.Coffee_Count++;
         }
         else{
-            System.out.println("\nAvailable Coffee Power(Gram) "+String.format("%.1f",this.coffee_powder));
-            System.out.println("Available Milk(Liter) "+String.format("%.1f", this.milk));
-            System.out.println("Available Water(Liter) "+String.format("%.1f", this.water));
-            System.out.println("\nSome Iteams Are Not Available, Please Fill before Making Coffee.");
+            System.out.println("\nKohvipuru on veel(grammi) "+String.format("%.1f",this.kohvi_puru));
+            System.out.println("Vett on veel(Liitrid) "+String.format("%.1f", this.vesi));
+            System.out.println("\nMõned komponendid on otsas, Palun täida enne kui kohvi teed.");
+        }
+    }
+
+    private void MilkCoffee(){
+        if(this.kohvi_puru >= 10 && this.piim >= 0.4 && this.vesi >= 0.2){
+            System.out.println("\nTeen piimakohvi...");
+            System.out.println("\nVõtan 10gm kohvipuru.");
+            this.kohvi_puru = this.kohvi_puru - 10;
+            System.out.println("Lisan 0.4 liitrit piima.");
+            this.piim = this.piim - 0.4;
+            System.out.println("Lisan 0.2 liitrit vett.");
+            this.vesi = this.vesi - 0.2;
+            System.out.println("\nSinu piimakohv on valmis.");
+            this.Coffee_Count++;
+        }
+        else{
+            System.out.println("\nKohvipuru on veel(grammi) "+String.format("%.1f",this.kohvi_puru));
+            System.out.println("Piima on veel(Liitrid) "+String.format("%.1f", this.piim));
+            System.out.println("Vett on veel(Liitrid) "+String.format("%.1f", this.vesi));
+            System.out.println("\nMõned komponendid on otsas, Palun täida enne kui kohvi teed.");
         }
     }
     public void start(){    //public Start to accese all private method of this class
         System.out.println(" ----------------------------------------------------------------");
-        System.out.println("|                   Coffee Machine By Manikant                   |");
+        System.out.println("|                   Kristi kohvimasin               |");
         System.out.println(" ----------------------------------------------------------------");
         //System.out.println("Currently We Have Following Operations, Select as your Need:- ");
-        System.out.println("\nCurrent Status: ");
+        System.out.println("\nHetkene staatus: ");
         this.GetIngredient();
         boolean t = true;
         while(t){
             System.out.println("\n -------------------------------- ");
-            System.out.println("|1:     Status of Ingredient     |\n -------------------------------- \n|2:      Fill Ingredient         |\n -------------------------------- \n|3:       Clean Machine          |\n -------------------------------- \n|4:        Make Coffee           |\n -------------------------------- \n|5: How many Coffee We have made?|\n -------------------------------- \n|6:        Exit                  |");
+            System.out.println("|1:     Komponentide staatus     |\n -------------------------------- \n|2:      Täida kohvimasinat         |\n -------------------------------- \n|3:       Masinahooldus          |\n -------------------------------- \n|4:        Tee kohvi          |\n -------------------------------- \n|5: Mitu kohvi on tehtud?|\n -------------------------------- \n|6:        Lülita välja                  |");
             System.out.println(" -------------------------------- \n\n");
             char c = CoffeeMachine.scan.next().charAt(0);
             switch(c){
                 case '1':
-                    System.out.println("------------- Status ------------");
+                    System.out.println("------------- Staatus ------------");
                     this.GetIngredient();
                     System.out.println("---------------------------------");
                     break;
@@ -110,10 +132,10 @@ class CoffeeMachine{
                     this.makecoffee();
                     break;
                 case '5':
-                    System.out.println("\nWe Have Made "+this.Coffee_Count+" Coffees.");
+                    System.out.println("\nOlen teinud "+this.Coffee_Count+" kohvit.");
                     break;
                 case '6':
-                    System.out.println("\nExiting...\n");
+                    System.out.println("\nTa-daa, lähen tuttu...\n");
                     t = false;
                     break;
             }
--- week10/MakeCoffee.java
+++ week10/MakeCoffee.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class MakeCoffee{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("\nWants to Start Mechine Y or N ?");
+        System.out.println("\nTeeme kohvi või ei J or E ?");
         char d;
         d = scan.next().charAt(0);
-        if(d == 'Y' || d == 'y'){
+        if(d == 'J' || d == 'j'){
             CoffeeMachine cm = new CoffeeMachine();     //Instances For CoffeMachine
             cm.start();     //In order to call All Private Method Calling Public mathod
-            System.out.println("Shutting Down...\n");
+            System.out.println("Lülitan välja...\n");
         }else
-            System.out.println("Shutting Down...\n");
+            System.out.println("Lülitan välja...\n");
     }
 }
